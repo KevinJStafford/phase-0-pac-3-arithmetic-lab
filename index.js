d2b1c064c8ead1fee6ff506c2782d3e3902c5129
Kevin <staffordkevinjames@gmail.com>
2023-08-16 20:34:49 -0400

    complete lab

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b){
+    return a / b;
+}
+
+function increment(a) {
+    return a + 1;
+}
+
+function decrement(a) {
+    return a - 1;
+}
+
+function makeInt(a) {
+    return parseInt(a, 10);
+}
+
+function preserveDecimal(a) {
+    return parseFloat(a);
+}
